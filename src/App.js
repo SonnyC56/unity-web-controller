fb15da4deccd894d79e59765637f9bfa1a32926d
SonnyC56 <cbcirasu@syr.edu>
2023-03-26 00:56:51 -0400

    websocket and joystick connected

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import useWebSocket from 'react-use-websocket';
+import { Joystick } from 'react-joystick-component';
+
+
+const WS_URL = 'ws://localhost:8080';
 
 function App() {
+
+  const sendMessage = (message) => {
+    const payload = { message: message };
+    sendJsonMessage(payload);
+  };
+
+  const { sendJsonMessage } = useWebSocket(WS_URL, {
+    onOpen: () => {
+      console.log('WebSocket connection established.');
+    },
+    onMessage: (event) => {},
+    onError: (event) => {},
+  });
+
+
+  const handleMove = (data) => {
+    sendJsonMessage(data);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       <h1> Unity Web Controller</h1>
+        <Joystick size={100} baseColor="red" stickColor="blue" move={handleMove}  ></Joystick>
+        <div className="arrow-buttons">
+          <button onClick={() => sendMessage('Jump')}>Jump</button>
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tap the buttons to move the unity camera
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
