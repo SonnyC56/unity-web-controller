4e2c6a5767f81b2ad495050eadac579937715454
SonnyC56 <cbcirasu@syr.edu>
2023-04-30 03:50:31 -0400

    better queing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import useWebSocket from 'react-use-websocket';
 import { Joystick } from 'react-joystick-component';
 
@@ -6,14 +6,36 @@ const WS_URL = 'ws://localhost:8090';
 
 function App() {
   const [hasControl, setHasControl] = useState(false);
+  const [queuePosition, setQueuePosition] = useState(0);
+  const [totalClients, setTotalClients] = useState(0);
+
+  console.log('app rendered')
+  const didMount = useRef(false);
 
   const { sendJsonMessage } = useWebSocket(WS_URL, {
+    onOpen: () => {
+      if (!didMount.current) {
+      // Send a message to the server to indicate that this is a web client
+      sendJsonMessage({ type: "join" });
+      console.log('join message sent')
+         didMount.current = true;
+      }
+    },
     onMessage: (event) => {
       const data = JSON.parse(event.data);
       console.log('recieving data ' , data)
       if (data.type === "control") {
         // This client has been granted control of the camera
         setHasControl(true);
+        setQueuePosition(0);
+      } else if (data.type === "queue") {
+        // Update the total number of clients and the position of this client in the queue
+        setTotalClients(data.totalClients);
+        setQueuePosition(data.position);
+      } else if (data.type === "done") {
+        // The current client is done controlling the camera
+        setHasControl(false);
+        setQueuePosition(0);
       }
     },
   });
@@ -41,7 +63,13 @@ function App() {
             <button onClick={handleDone}>Done</button>
           </>
         ) : (
-          <p>Waiting for control...</p>
+          <p>
+            {queuePosition > 0 ? (
+              `Waiting for control. Position in queue: ${queuePosition} / ${totalClients/2}`
+            ) : (
+              "Waiting for control..."
+            )}
+          </p>
         )}
       </header>
     </div>
